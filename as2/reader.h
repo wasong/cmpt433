b11b96ec3e37d084a9f19e1ae2734bd338e00eab
Andrew Song <andrewgnos@gmail.com>
2017-10-10 18:08:34 -0700

    modular files

---
+++ as2/reader.h
@@ -0,0 +1,13 @@
+// reader.h
+// represents the Potentiometer reader
+
+#ifndef READER_H_
+#define READER_H_
+
+#define A2D_FILE_VOLTAGE0 "/sys/bus/iio/devices/iio:device0/in_voltage0_raw"
+#define A2D_VOLTAGE_REF_V 1.8
+#define A2D_MAX_READING 4095
+
+typedef struct {
+	int getVoltage0Reading();
+} PotReader;
