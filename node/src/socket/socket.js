60f339df610004c09a85ee50d6e28a99581585fc
Andrew Song <andrewgnos@gmail.com>
2017-10-30 17:42:35 -0700

    udp server

--- node/src/socket/socket.js
+++ node/src/socket/socket.js
@@ -1,9 +1,13 @@
 import socketIO from 'socket.io'
 
+import handleCommand from './UDPServer'
+
 const listener = (server) => {
   const io = socketIO(server)
   io.on('connection', (socket) => {
     console.log('user connected')
+
+    handleCommand(server)
 
     socket.on('message', (msg) => {
       console.log(`message: ${msg}`)
