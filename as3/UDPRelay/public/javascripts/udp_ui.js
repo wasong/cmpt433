24115834eb2fdb573d0012548f57b5f3a5b88130
Andrew Song <andrewgnos@gmail.com>
2017-11-25 19:55:13 -0800

    loop through images

--- as3/UDPRelay/public/javascripts/udp_ui.js
+++ as3/UDPRelay/public/javascripts/udp_ui.js
@@ -2,73 +2,32 @@
 // Client-side interactions with the browser.
 
 // Make connection to server when web page is fully loaded.
-var socket = io.connect();
-var monitors = setInterval(function() {
-	sendCommand("volume_monitor", "getVolume\n");
-	sendCommand("tempo_monitor", "getTempo\n");
-	sendCommand("beat_monitor", "getBeat\n");
-}, 1000)
+// var socket = io.connect();
 
+var imagePos = 0
+var url = "https://localhost:8088/grabber0"
 
 $(document).ready(function() {
-	setInterval(function()	{
-		var newTimestamp = new Date();
-		var newImageSrc	= "/images/<DA_IMG_NAME>.jpg?time=" + newTimestamp.getTime();
-		$DA_IMG_TAG.removeAttr("src").attr("src", newImageSrc);
-	}, 1000);
-
-	$('#help').click(function(){
-		sendCommand("prime", "help\n");
-	});
-	$('#volUp').click(function(){
-		sendCommand("prime", "volumeI\n");
-	});
-	$('#volDown').click(function(){
-		sendCommand("prime", "volumeD\n");
-	});
-	$('#tempoUp').click(function(){
-		sendCommand("prime", "tempoI\n");
-	});
-	$('#tempoDown').click(function(){
-		sendCommand("prime", "tempoD\n");
-	});
-	$('#beatNext').click(function(){
-		sendCommand("prime", "beatN\n");
-	});
-	$('#beatPrev').click(function(){
-		sendCommand("prime", "beatP\n");
-	});
-	$('#beat1').click(function(){
-		sendCommand("prime", "beat1\n");
-	});
-	$('#beat2').click(function(){
-		sendCommand("prime", "beat2\n");
-	});
-	$('#beat3').click(function(){
-		sendCommand("prime", "beat3\n");
-	});
-	
-	socket.on('commandReply', function(result) {
-		var newDiv = $('<div></div>').text(result);
-		$('#console').append(newDiv);
-		$('#console').scrollTop($('#console').prop('scrollHeight'));
-	});
-	socket.on('volume_monitor_reply', function(result) {
-			updateMonitor("#currentVolume", result);
-		});
-	socket.on('tempo_monitor_reply', function(result) {
-			updateMonitor("#currentTempo", result);
-		});
-	socket.on('beat_monitor_reply', function(result) {
-		updateMonitor("#currentBeatMode", result);
-	});
+	$("#getImage").click(function() {
+		setInterval(showImages, 250)
+	})
 });
 
-function updateMonitor(key, value) {
-	var newDiv = $('<span></span>').text(value);
-	$(key).html(newDiv);
+function showImages() {
+	if (imagePos < 20) {
+		imagePos += 1
+		setImage()
+	} else {
+		imagePos = 1
+		setImage()
+	}
 }
 
-function sendCommand(command, message) {
-	socket.emit(command, message);
-};
+function setImage() {
+	var path = imagePos < 10 ?
+	url + "0" + imagePos + ".jpeg" :
+	url + imagePos + ".jpeg"
+
+	$("#webcam").attr("src", path)
+}
+
