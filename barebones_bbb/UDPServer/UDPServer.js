4ee61556e12dcec6243cf97a2c3413c4e6f582d0
Andrew Song <andrewgnos@gmail.com>
2017-12-03 22:01:10 -0800

    handle different commands

--- barebones_bbb/UDPServer/UDPServer.js
+++ barebones_bbb/UDPServer/UDPServer.js
@@ -6,12 +6,20 @@ const socketio = require('socket.io')
 const dgram = require('dgram')
 
 const handleUDPConnection = (socket, command, replyCommand) => {
-  socket.on(command, (data, args = '') => {
+  socket.on(command, (data, args = null) => {
     console.log(`'Command: ${data} ${args}`)
     // Info for connecting to the local process via UDP
     const PORT = 12345
     const HOST = '192.168.7.2'
-    const buffer = new Buffer(`${data} ${args.length ? args.length : ''} ${args}`)
+    let bufferData = null
+
+    if (data === 'setCode') {
+      bufferData = `${data} ${args.length ? args.length : ''} ${args}`
+    } else {
+      bufferData = data
+    }
+
+    const buffer = new Buffer(bufferData)
 
     const client = dgram.createSocket('udp4')
     client.send(buffer, 0, buffer.length, PORT, HOST, (err, bytes) => {
